610a1557ebad5898e115bd6ca224330201312a76
jsantis <jsantis@fen.uchile.cl>
2021-05-11 18:11:46 -0400

    arregla img

--- SGD/urls.py
+++ SGD/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
