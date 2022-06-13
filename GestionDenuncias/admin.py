ee4959f24be90d36b9ab39971e5952f73c7f4b91
jsantis <jsantis@fen.uchile.cl>
2022-06-13 00:10:53 -0400

    SAF - Actualiza Admin

--- GestionDenuncias/admin.py
+++ GestionDenuncias/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 from GestionRecursos.models import *
+from GestionPAS.models import *
+from AlertasFiscalizacion.models import *
 
 admin.site.register(Denuncias)
 admin.site.register(Abogados)
@@ -14,3 +16,4 @@ admin.site.register(Aportes)
 admin.site.register(Cartola)
 admin.site.register(Recursos)
 admin.site.register(UsersRecursos)
+admin.site.register(Bitacora)
