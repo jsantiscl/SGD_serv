6bf500c832574f47c181dc88ccde962239f8eb49
jsantis <jsantis@fen.uchile.cl>
2023-03-22 18:04:47 -0300

    SCP - flujo hasta auditor

--- SistemaControlPreventivo/urls.py
+++ SistemaControlPreventivo/urls.py
@@ -3,8 +3,13 @@ from django.urls import include
 from django.contrib.auth.decorators import login_required
 from SistemaControlPreventivo.views import *
 urlpatterns = [
-    # Fiscalizacion
+    # ENCARGADO
     path('asignacioncandidatos/', login_required(admin_asignacion_candidato), name='admin_asignacion_candidato'),
     path('asignacionpartidos/', login_required(admin_asignacion_partido), name='admin_asignacion_partido'),
     path('asignacandidatos/', login_required(asignar_candidatos_scp), name='asignar_candidatos_scp'),
+    path('asignapartidos/', login_required(asignar_partidos_scp), name='asignar_partidos_scp'),
+    # Auditor
+    path('candidatos/', login_required(auditor_candidatos), name='auditor_candidatos'),
+    path('partidos/', login_required(auditor_partidos), name='auditor_partidos'),
+    path('pasaretapa/', login_required(pasaretapa), name='pasaretapa'),
 ]
--- SistemaControlPreventivo/views.py
+++ SistemaControlPreventivo/views.py
@@ -46,3 +46,59 @@ def admin_asignacion_partido(request):
     context = {'candidatos': candidatos, 'auditores': auditores}
 
     return render(request, 'SistemaControlPreventivo/SCP_Admin_Asignacion_Partidos.html', context)
+
+
+def auditor_candidatos(request):
+    # Consulta el grupo por su nombre
+    group = Group.objects.get(name="AuditorControlPreventivo")
+
+    # Obtiene todos los usuarios que pertenecen al grupo
+    auditores = group.user_set.all()
+    usuario_actual=request.user.username
+    # Filtrar los candidatos como lo hacías antes
+    candidatos = Candidatos.objects.filter(estado='2_AsignadoAuditor', asignado_a=usuario_actual)
+
+    # Agregar los auditores al contexto
+    context = {'candidatos': candidatos, 'auditores': auditores}
+
+    return render(request, 'SistemaControlPreventivo/SCP_Auditor_Candidatos.html', context)
+
+def auditor_partidos(request):
+    # Consulta el grupo por su nombre
+    group = Group.objects.get(name="AuditorControlPreventivo")
+
+    # Obtiene todos los usuarios que pertenecen al grupo
+    auditores = group.user_set.all()
+    usuario_actual=request.user.username
+    # Filtrar los candidatos como lo hacías antes
+    partidos = Partidos.objects.filter(estado='2_AsignadoAuditor', asignado_a=usuario_actual)
+    print(usuario_actual)
+    # Agregar los auditores al contexto
+    context = {'partidos': partidos, 'auditores': auditores}
+
+    return render(request, 'SistemaControlPreventivo/SCP_Auditor_Partidos.html', context)
+
+def pasaretapa(request):
+    data = json.loads(request.body)
+    if data['datos']['tipo'] == 'Candidato':
+         Candidatos.objects.filter(rut=str(data['datos']['rut'])).update(estado=str(data['datos']['etapa']))
+         WorkflowSCP.objects.create(rut_candidato_partido=str(data['datos']['rut']), usuario=str(request.user),
+                                                nueva_etapa=str(data['datos']['etapa']),
+                                                fecha_cambio=datetime.now())
+    if data['datos']['tipo'] == 'Partido':
+         Partidos.objects.filter(par_rut=str(data['datos']['rut'])).update(estado=str(data['datos']['etapa']))
+         WorkflowSCP.objects.create(rut_candidato_partido=str(data['datos']['rut']), usuario=str(request.user),
+                                                nueva_etapa=str(data['datos']['etapa']),
+                                                fecha_cambio=datetime.now())
+    return JsonResponse([str(data['datos']['rut']), 'Asignado'], safe=False)
+
+def asignar_partidos_scp(request):
+    data = json.loads(request.body)
+    if data['datos']['asignacion'] != 'Pendiente':
+         Partidos.objects.filter(par_rut=str(data['datos']['rut'])).update(asignado_a=str(data['datos']['asignacion']))
+         Partidos.objects.filter(par_rut=str(data['datos']['rut'])).update(estado='2_AsignadoAuditor')
+         WorkflowSCP.objects.create(rut_candidato_partido=str(data['datos']['rut']), usuario=str(data['datos']['asignacion']),
+                                                nueva_etapa='2_AsignadoAuditor',
+                                                fecha_cambio=datetime.now())
+    return JsonResponse([str(data['datos']['rut']), 'Asignado'], safe=False)
+
